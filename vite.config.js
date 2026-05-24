420c27e2d5bf686540d15aff26ab455d84ee507a
Natalie Kizirian <natalie.kizirian@gmail.com>
2026-05-24 18:29:07 +0300

    fix base url for netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-// import tailwindcss from "@tailwindcss/vite";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/Client-Tracker-v2/",
+  base: "/", 
   plugins: [
     react(),
     tailwindcss(),
