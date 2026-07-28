a5b93936d446e5b63e27f378a3b7a1b2ea93e84e
Natalie Kizirian <natalie.kizirian@gmail.com>
2026-07-28 10:09:22 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Client-Tracker-v2/sw.js', { scope: '/Client-Tracker-v2/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
